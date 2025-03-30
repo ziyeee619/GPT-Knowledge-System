f29f5a0337d0521d8323510bf730604a79e5a2c3
ziyeee619 <ziyechan619@gmail.com>
2025-03-30 15:09:21 +0800

    Add files via upload

---
+++ src/components/Collaboration.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Collaboration() {
+  return <h1>Collaboration Page</h1>;
+}
+
+export default Collaboration;
---
+++ src/components/KnowledgeBase.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function KnowledgeBase() {
+  return <h1>Knowledge Base Page</h1>;
+}
+
+export default KnowledgeBase;
